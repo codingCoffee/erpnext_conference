08ff616beb6bb90bc73197496b3c3114339b2626
Ameya Shenoy <shenoy.ameya@gmail.com>
2017-06-29 17:26:07 +0530

    abstraction complete, beautification and modularity left --conf-site

--- erpnext_conference/erpnext_conference/web_form/buy_tickets/buy_tickets.js
+++ erpnext_conference/erpnext_conference/web_form/buy_tickets/buy_tickets.js
@@ -1,13 +1,60 @@
 frappe.ready(function() {
-	$('.form-group:last').after( bookingTitle + bookingTicket + endDiv);	
+	$('#page-buy-tickets > div > div.page-content.with-sidebar.col-sm-9 > div.page_content > form > div > div.section > div > div > p').after( bookingTitle + bookingTicket + endDiv + saveButton);	
 
 	for (var i=1; i<=3; i++){
 		autoCostEdit(i);
 	}
 
+	// Hide the save buttom
+	$("div#page-buy-tickets div > button[type='submit']").hide()
+	$("div#page-buy-tickets p > button[type='submit']").hide()
+	$(".form-group~ .web-form-grid th , .btn-add-row , .form-group+ .small, thead td").hide()
 })
 
+var ticketTypes = ['Prime', 'Gold', 'Silver']
+var ticketCosts = [1000, 100, 10]
+var abstractTicketPointer = 2
 
+function isEverythingAllRight() {
+	if (!(/^[a-zA-Z ]+$/.test($("input[name='participant_name']").val()))){
+		alert("Lol fix your name")
+		return false;
+	}
+	if (!(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test($("input[name='email']").val()))){
+		alert("Lol fix your email")
+		return false;
+	}
+	if ($("div#page-buy-tickets tr:nth-child(4) > td:nth-child(2) > input[type='text']").val() == '0'){
+		alert('buy tickets before confirming your order');
+		return false;
+	}
+	return true;
+}
+
+function realSaveTest() {
+	if (isEverythingAllRight()){
+		// keep a function above and check if all fields are correctly filled
+		autoPopulateTable();
+		$("div#page-buy-tickets p > button[type='submit']").click();
+	}	
+}
+
+
+function autoPopulateTable() {
+	for (var i=1; i<=3; i++) {
+		if ($("div#page-buy-tickets tr:nth-child("+i+") > td:nth-child(3) > input#number").val() > 0) {
+			// alert('hi')
+			$("div#page-buy-tickets div.section > div > div > p > button").click();
+			$("div#page-buy-tickets tr:nth-child("+abstractTicketPointer+") > td:nth-child(1) > div > select").val(ticketTypes[i-1]);
+			$("div#page-buy-tickets tr:nth-child("+abstractTicketPointer+") > td:nth-child(2) > div > input[name='cost']").val(ticketCosts[i-1]);
+			$("div#page-buy-tickets tr:nth-child("+abstractTicketPointer+") > td:nth-child(3) > div > input[name='quantity']").val(parseInt($("div#page-buy-tickets tr:nth-child("+i+") > td:nth-child(3) > input#number").val()));
+			abstractTicketPointer++;
+		}
+	}
+}
+
+
+// Calculates the sub total for each type of ticket
 function autoCostEdit(a) {
 	console.log("frappe a: "+a);
 	$("div#page-buy-tickets tr:nth-child("+a+") > td:nth-child(3) > input[type='text']").change(function () {
@@ -17,6 +64,8 @@ function autoCostEdit(a) {
 	})
 }
 
+
+// Calculates and populated the final total field automatically 
 function autoTotalEdit() {
 	var finTotal = 0;
 	for (var i=1; i<=3; i++){
@@ -25,12 +74,18 @@ function autoTotalEdit() {
 	$("div#page-buy-tickets tr:nth-child(4) > td:nth-child(2) > input[type='text']").val(finTotal);
 }
 
+
+// Booking Title framework
 var bookingTitle =
 "<div class='form-group'> \
 	<label for='status' class='control-label text-muted small'>Booking</label>"
 
+
+// End the div tag
 var endDiv = "</div>"
 
+
+// Booking Ticket Table
 var bookingTicket = 
 "<div class='web-form-grid' data-fieldname='tickets' data-doctype='Participant Ticket'> \
    <table class='table table-bordered'> \
@@ -102,11 +157,11 @@ var bookingTicket =
 </div>"
 
 
-// Select your input element.
-var number = document.getElementById('number');
-
-
-
-// formatting 
-// default value
-// onkeypress='return (event.charCode == 8 || event.charCode == 0 || event.charCode == 13) ? null : event.charCode >= 48 && event.charCode <= 57'
+var saveButton = 
+`<div> \
+	<br> \
+	<p class='text-right'> \
+		<button id='saveButton' onclick='realSaveTest()' class='btn btn-primary btn-sm btn-form-submit'> \
+			Save</button> \
+	</p> \
+</div>`
